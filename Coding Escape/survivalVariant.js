c93f07a9b2173e115a9f3ca94dad5d1635cac319
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-01 07:05:48 -0700

    updated file

---
+++ Coding Escape/survivalVariant.js
@@ -0,0 +1,18 @@
+const n = parseInt(readline);
+const d = parseInt(readline);
+const h = parseInt(readline);
+
+// Calculate the total number of hoursneeded to survive 
+const totalHoursNeeded = d * 24;
+
+// Calculate the total number of hours the aplles can provide 
+
+const totalHoursFromApples = n* h ;
+
+if (totalHoursFromApples > totalHoursNeeded) {
+    console.log("It is enough");
+} else if (totalHoursFromApples === totalHoursNeeded) {
+    console.log("Just enough");
+} else {
+    console.log("Not enough");
+}
