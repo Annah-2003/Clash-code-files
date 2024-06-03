b99d78d49a62ab1a03f302a19a0262addafaa1dc
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-03 04:25:12 -0700

    created a trigger for the rune

--- Coding Escape/robotSimulation.js
+++ Coding Escape/robotSimulation.js
@@ -5,5 +5,27 @@ let commands =[];
 
 // Function to move Blub to a specific zonw
 function moveToZone(targetZone){
-    
+    while (currentZone !== targetZone){
+        if(targetZone > currentZone){
+        commands.push('>');
+        currentZone++;
+        }
+        else{
+            commands.push('<')
+            currentZone--;
+        }
+    }
+
+}
+
+//Function to get the minimum steps to reach target character from current character
+function getMinSteps(currentChar, targetChar) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ';
+    const currentIndex = alphabet.indexOf(currentChar);
+    const targetIndex = alphabet.indexOf(targetChar);
+
+    let forwardSteps = targetIndex >= currentIndex ? targetIndex - currentIndex : 27 - (currentIndex - targetIndex);
+    let backwardSteps = currentIndex >= targetIndex ? currentIndex - targetIndex : 27 - (targetIndex - currentIndex);
+
+    return forwardSteps <= backwardSteps ? ['+', forwardSteps] : ['-', backwardSteps];
 }
