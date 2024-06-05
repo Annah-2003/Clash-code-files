fc2054054bfc5bb14edc8f3119990ec805cb06c3
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-05 07:34:14 -0700

    updated consition statement

--- Coding Escape/stockInvestments.js
+++ Coding Escape/stockInvestments.js
@@ -7,6 +7,19 @@ let maxValue = values[0];
 for (let i = 0; i < n; i++) {
     const currentValue = values[i];
     
-}
+
 
 //calculate potential loss
+const potentialLoss = maxValue - currentValue;
+
+//update maxLoss if we found a bigger loss
+if (potentialLoss > maxLoss) {
+    maxLoss = potentialLoss
+}
+
+// Update maxValue if the currentValue is higher than  the maxValue 
+if (currentValue > maxValue){
+    maxValue = currentValue
+}
+}
+console.log(maxLoss === 0? 0 : - maxLoss);
