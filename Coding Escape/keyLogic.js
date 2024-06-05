688b2670315956ea7114c54a4898934bf54c0401
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-05 06:39:34 -0700

    added a new file

--- Coding Escape/keyLogic.js
+++ Coding Escape/keyLogic.js
@@ -7,3 +7,42 @@ const exitPos = parseInt(inputs[4]);
 const nbTotalCones = parseInt(inputs[5]);
 const nbAdditionalElevators = parseInt(inputs[6]);
 const nbElevators = parseInt(inputs[7]);
+
+// store elevator positions 
+const elevators = {};
+for (let i = 0; i < nbElevators; i++) {
+    var inputs = readline().split(' ');
+    const elevatorFloor = parseInt(inputs[0]); // floor on which this elevator is found
+    const elevatorPos = parseInt(inputs[1]); // position of the elevator on its floor
+    elevators[elevatorFloor] = elevatorPos;
+    
+}
+
+// game loop
+while (true) {
+    var inputs = readline().split(' ');
+    const cloneFloor = parseInt(inputs[0]); // floor of the leading clone
+    const clonePos = parseInt(inputs[1]); // position of the leading clone on its floor
+    const direction = inputs[2]; // direction of the leading clone: LEFT or RIGHT
+
+    if (cloneFloor === -1) {
+        // No clone to control, so wait
+        console.log('WAIT');
+        continue;
+} 
+// Determine the target position
+let targetPos;
+if (cloneFloor === exitFloor) {
+    targetPos = exitPos;
+} else {
+    targetPos = elevators[cloneFloor];
+}
+
+// Decide whether to block or wait
+if ((direction === "RIGHT" && clonePos > targetPos) ||
+    (direction === "LEFT" && clonePos < targetPos)) {
+    console.log('BLOCK');
+} else {
+    console.log('WAIT');
+}
+}
---
+++ Coding Escape/pathFindingGolf.js
@@ -0,0 +1,6 @@
+// read input dimensions
+var inputs = readline().split(' ');
+const width = parseInt(inputs[0]);
+const height = parseInt(inputs[1]);
+
+//read the grid
