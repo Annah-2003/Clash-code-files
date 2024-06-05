de1bda19e63a7606d479b15d14140c3a4dc8a3e0
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-05 07:26:17 -0700

    create a stock investment file

---
+++ Coding Escape/stockInvestments.js
@@ -0,0 +1,12 @@
+const n = parseInt(readline());
+const values = readline().split(' ').map(Number);
+
+let maxLoss = 0;
+let maxValue = values[0];
+
+for (let i = 0; i < n; i++) {
+    const currentValue = values[i];
+    
+}
+
+//calculate potential loss
