c573116c2915b29b6f2b50e23307baf9774e77ae
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-29 06:51:34 -0700

    created new file

---
+++ Coding Escape/theBridge.py
@@ -0,0 +1,70 @@
+import sys
+
+def get_lane(lane_index):
+    return lanes[lane_index]
+
+def can_jump(x, y, s):
+    lane = get_lane(y)
+    for i in range(1, s+1):
+        if lane[x+i] == '0':
+            return False
+    return True
+
+def can_move_up(y):
+    return y > 0
+
+def can_move_down(y):
+    return y < 3
+
+def will_fall(x, y, s):
+    lane = get_lane(y)
+    for i in range(1, s+1):
+        if lane[x+i] == '0':
+            return True
+    return False
+
+def safe_to_speed(x, y, s):
+    return not will_fall(x, y, s + 1)
+
+def safe_to_slow(x, y, s):
+    return s > 1 and not will_fall(x, y, s - 1)
+
+# Read initialization input
+m = int(input())  # the amount of motorbikes to control
+v = int(input())  # the minimum amount of motorbikes that must survive
+lanes = [input() for _ in range(4)]  # L0 to L3 are lanes of the road.
+
+# game loop
+while True:
+    s = int(input())  # the motorbikes' speed
+    bikes = []
+    for i in range(m):
+        x, y, a = [int(j) for j in input().split()]
+        if a == 1:
+            bikes.append((x, y, a))
+
+    # Determine the action to take
+    action = "WAIT"
+    for bike in bikes:
+        x, y, a = bike
+        
+        # Check if we need to jump
+        if will_fall(x, y, s):
+            if can_jump(x, y, s):
+                action = "JUMP"
+            elif can_move_up(y) and not will_fall(x, y - 1, s):
+                action = "UP"
+            elif can_move_down(y) and not will_fall(x, y + 1, s):
+                action = "DOWN"
+            else:
+                action = "SLOW"
+            break
+        
+        # Check if we can safely speed up
+        if safe_to_speed(x, y, s):
+            action = "SPEED"
+        elif safe_to_slow(x, y, s):
+            action = "SLOW"
+
+    # Output the chosen action
+    print(action)
