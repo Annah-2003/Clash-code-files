c065d04d821703bcf78c0f3266e3c117a3878fed
Annah-2003 <irene.gitau@strathmore.edu>
2024-07-01 11:15:56 -0700

    created  new file

--- Coding Escape/theDescent.py
+++ Coding Escape/theDescent.py
@@ -5,4 +5,17 @@ import math
 while True:
     max_height = -1
     target_index = -1
-     
+
+
+    for i in range(8):
+        mountain_h = int(input()) # represents the height of one mountain.
+
+
+        # Find the highest mountain
+        if mountain_h > max_height:
+            max_height = mountain_h
+            target_index = i
+
+
+        # Prin the index of the mountain to fire on
+        print(target_index)
