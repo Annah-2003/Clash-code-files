7529aec414151f1da0c3355d8f8991de82013088
Annah-2003 <irene.gitau@strathmore.edu>
2024-07-01 23:53:59 -0700

    updates savehumans and zobies file

--- Coding Escape/theDescent.py
+++ Coding Escape/theDescent.py
@@ -1,21 +1,18 @@
 import sys
-import math 
+import math
 
 # game loop
 while True:
     max_height = -1
     target_index = -1
-
-
+    
     for i in range(8):
-        mountain_h = int(input()) # represents the height of one mountain.
-
-
+        mountain_h = int(input())  # represents the height of one mountain.
+        
         # Find the highest mountain
         if mountain_h > max_height:
             max_height = mountain_h
             target_index = i
 
-
-        # Prin the index of the mountain to fire on
-        print(target_index)
+    # Print the index of the mountain to fire on
+    print(target_index)
